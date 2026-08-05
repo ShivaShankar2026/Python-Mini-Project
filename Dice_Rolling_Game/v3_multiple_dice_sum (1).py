58b91d378d6c12cbb0084c46f7abed8fdde887ff
ShivaShankar2026 <kshiva2004@zohomail.in>
2026-08-05 22:06:03 +0530

    Update v3_multiple_dice_sum (1).py

--- Dice_Rolling_Game/v3_multiple_dice_sum (1).py
+++ Dice_Rolling_Game/v3_multiple_dice_sum (1).py
@@ -1,6 +1,5 @@
 # Multiple Dice Roller with Sum
 
-
 import random
 
 playing = True
@@ -8,22 +7,22 @@ count = 0
 sum_of_dice = 0
 
 while True:
-    # User Choice
+     # User Choice
     choice = input("roll the dice? (y/N)").lower()
 
-    # Roll Dice
+     # Roll Dice
     if choice == 'y' :
         count += 1
         
-        # Number of Dice 
+         # Number of Dice 
         num = int(input("Enter the no of dices want to roll"))
         
-        # Check Valid Number
+         # Check Valid Number
         if num <= 0:
             print("Invalid Number")
             num = int(input("Enter the no of dices want to roll"))
         
-        # Generates The Dice Roll
+         # Generates The Dice Roll
         for i in range(num):
             dice = random.randint(1,6)
             print(f' Dice {i+1}: {dice} ')
@@ -31,12 +30,12 @@ while True:
         print(f"Sum of dice = {sum_of_dice} ")
             
             
-    # Exit game   
+     # Exit game   
     elif choice == 'n':
         print('Thanks for playing')
         print(f"You have played {count} times")
         break
 
-     # Invalid choice   
+      # Invalid choice   
     else:
         print("Invalid choice")
