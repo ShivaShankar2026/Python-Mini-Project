fd24dfca7ac8de2c927c41efc108b1987fd1ec27
ShivaShankar2026 <kshiva2004@zohomail.in>
2026-08-06 19:17:24 +0530

    Update v4_dice_roller_history (1).py

--- Dice_Rolling_Game/v4_dice_roller_history (1).py
+++ Dice_Rolling_Game/v4_dice_roller_history (1).py
@@ -1,6 +1,5 @@
 # Dice Roller With History
 
-
 import random
 
 playing = True
@@ -9,46 +8,46 @@ all_rolls = {}
 
 
 while playing:
-    # User choice
+     # User choice
     choice = input('Roll the Dice (y/n): ').lower()
     
-    # Roll dice
+     # Roll dice
     if choice == 'y':
         counter += 1
 
-        # Number of dice
+         # Number of dice
         num = int(input('Enter the no fo dice want to roll: '))
 
-        # Store current roll
+         # Store current roll
         rolls = []
-
-        # Check valid number
+ 
+         # Check valid number
         if num <= 0:
             print('Invalid number')
             num = int(input('Enter the no fo dice want to roll: '))
             
-        # Generate dice rolls
+         # Generate dice rolls
         for i in range(num):
             dice = random.randint(1,6)
             rolls.append(dice)
 
-        # Save roll history
+         # Save roll history
         all_rolls[f"Roll{counter}"] = rolls.copy()
 
-        # Display current roll
+         # Display current roll
         print(f'Roll{counter} -> {all_rolls[f"Roll{counter}"]}')
         
-    # Exit game 
+     # Exit game 
     elif choice == 'n':
         print(f'You have played {counter} times')
 
-        # Display Roll History
+          # Display Roll History
         for roll_name, values in all_rolls.items():
             print(f"{roll_name} -> {values}")
 
         print("\nThank you for playing")
         break
         
-    # Invalid Choice   
+      # Invalid Choice   
     else:
         print('Invalid choice')
